092b5d4926e79209bdd5461162901f0a868b5bfb
Taaseen <tali2@stuy.edu>
2018-11-02 11:48:37 -0400

    Added

--- main.c
+++ main.c
@@ -10,12 +10,16 @@ int main(){
   //  struct stat *file = malloc(sizeof(struct stat));
   //  stat("./", file);
   DIR *d;
-  d = opendir("testdir");
+  d = opendir("./");
   struct dirent* file = readdir(d);
-  file = readdir(d);
-  file = readdir(d);
-  printf("%c\n", file->d_name[0]);
-  printf("%c", file->d_name[1]);
+  while (file) {
+    if (file->d_type == DT_DIR)
+      printf("dir: ");
+    for (int i = 0; file->d_name[i]; i++)
+      printf("%c", file->d_name[i]);
+    printf("\n");
+    file = readdir(d);
+  }
   //printf("%c", file->d_name[2]);
   //printf("%c", file->d_name[3]);
   return 0;
