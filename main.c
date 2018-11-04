58b4d192ee4e53a0fbd58f5d676d4059658c055d
Taaseen Ali <taaseen.ali89@gmail.com>
2018-11-04 16:48:56 -0500

    done

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <sys/types.h>
 #include <string.h>
 #include <sys/dir.h>
+#include <errno.h>
 
 void indent(int num_indents){
   for(int i=0; i<num_indents; i++)
     printf("    ");
 }
 
-void print_dir(char * dir, int dir_num){
+void rprint_dir(char * dir, int dir_num){
   DIR *d;
   d = opendir(dir);
    
@@ -29,7 +29,7 @@ void print_dir(char * dir, int dir_num){
 	printf("/");
 	printf("%s", file->d_name);
 	printf("\n");
-      	print_dir(file->d_name, dir_num+1);
+      	rprint_dir(file->d_name, dir_num+1);
       }
 
       else{
@@ -46,8 +46,26 @@ void print_dir(char * dir, int dir_num){
 
 }
 
-int main(int c, char*){
-  print_dir("./", 0);
+void print_dir(char * d){
+  rprint_dir(d,0);
+}
+
+int main(int argc, char* argv[]){
+  if(argc > 1)
+    print_dir(argv[1]);
+  else{
+    char *s = malloc(256);
+    printf("Enter a directory\n");
+    fgets(s, 256, stdin);
+    s[strlen(s)-1] = NULL;
+    DIR * d = opendir(s);
+    if(!d){
+      printf("Bad directory: %s\n", strerror(errno));
+      return 1;
+    }
+    print_dir(s);
+  }
+
   
   return 0;
 }
